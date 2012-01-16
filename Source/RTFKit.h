fda6f73034ed36e204c9672dce7b47e74862a272
Max Seelemann <max@the-soulmen.com>
2012-01-16 18:34:11 +0100

    [Code] Initial checkin
    Signed-off-by: Max Seelemann <max@the-soulmen.com>

---
+++ Source/RTFKit.h
@@ -0,0 +1,11 @@
+//
+//  RTFKit.h
+//  RTFKit
+//
+//  Created by Max Seelemann on 16.01.12.
+//  Copyright (c) 2012 The Soulmen. All rights reserved.
+//
+
+@interface RTFKit : NSObject
+
+@end
---
+++ Tests/RTFKitTests.m
@@ -0,0 +1,33 @@
+//
+//  RTFKitTests.m
+//  RTFKitTests
+//
+//  Created by Max Seelemann on 16.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RTFKitTests.h"
+
+@implementation RTFKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+	STAssertEquals(3, 4-1, @"UUUH");
+//    STFail(@"Unit tests are not implemented yet in RTFKitTests");
+}
+
+@end
