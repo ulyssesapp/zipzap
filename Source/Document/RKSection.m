f645b31dbfe4cdc0084165737bd98fe9b6a132b3
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-20 11:58:27 +0100

    [CODE] Adding empty initi for RKSection

--- Source/Document/RKSection.m
+++ Source/Document/RKSection.m
@@ -8,8 +8,37 @@
 
 #import "RKSection.h"
 
+// Possible types of headers and footers
+typedef enum {
+    RKFirstPage = 0,
+    RKLeftPage = 1,
+    RKRightPage = 2,
+    
+    RKHighestPage = 3
+}RKPageSelection;
+
 @implementation RKSection
+{
+    NSAttributedString* headers[RKHighestPage];
+    NSAttributedString* footers[RKHighestPage];
+}
 
 @synthesize content, numberOfColumns, numberOfFirstPage, pageNumberingStyle, sameHeaderForAllPages, sameFooterForAllPages;
+
+-(id)init
+{
+    self = [super init];
+    
+    if (self) {
+        numberOfColumns = 1;
+        numberOfFirstPage = 1;
+        pageNumberingStyle = RKPageNumberingDecimal;
+        
+        sameHeaderForAllPages = true;
+        sameFooterForAllPages = true;
+    }
+    
+    return self;
+}
 
 @end
