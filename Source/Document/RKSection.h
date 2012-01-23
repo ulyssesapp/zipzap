387c35ada692852ef43b5389468a9130e57fface
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-23 11:01:31 +0100

    [CODE] Added missing documentation

--- Source/Document/RKSection.h
+++ Source/Document/RKSection.h
@@ -13,15 +13,36 @@
  */
 @interface RKSection : NSObject
 
+/*!
+ @abstract Creates a new section based on a given content string
+ */
 + (id)sectionWithContent:(NSAttributedString *)content;
 
 - (id)init;
+
+/*!
+ @abstract Initializes a section with a given content
+ */
 - (id)initWithContent:(NSAttributedString *)content;
 
+/*!
+ @abstract Retruns the headline for a given page type
+ */
 - (NSAttributedString *)headerForPage:(RKPageSelectionMask)pageMask;
+
+/*!
+ @abstract Changes the headline for a given page type
+ */
 - (void)setHeader:(NSAttributedString *)header forPages:(RKPageSelectionMask)pageMask;
 
+/*!
+ @abstract Retruns the footer for a single or multiple page types
+ */
 - (NSAttributedString *)footerForPage:(RKPageSelectionMask)pageMask;
+
+/*!
+ @abstract Chanes the footer for a single or multiple page types
+ */
 - (void)setFooter:(NSAttributedString *)footer forPages:(RKPageSelectionMask)pageMask;
 
 /*!
