e6b1533b4e8ff005637228ecb0e6754f75f6724a
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-23 12:19:21 +0100

    [CODE] Updating global header

--- Source/RTFKit.h
+++ Source/RTFKit.h
@@ -5,7 +5,6 @@
 //  Created by Max Seelemann on 16.01.12.
 //  Copyright (c) 2012 The Soulmen. All rights reserved.
 //
-
-@interface RTFKit : NSObject
-
-@end
+#import "RKTypes.h"
+#import "RKSection.h"
+#import "RKDocument.h"
