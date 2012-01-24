ff2b9a82f059b769df8dab567901ef185ab31d22
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-24 11:56:41 +0100

    Creating RKAttributeCollector

---
+++ Source/Export/RKAttributeCollector.m
@@ -0,0 +1,13 @@
+//
+//  RKDefinitionsCollector.m
+//  RTFKit
+//
+//  Created by Friedrich Gräter on 23.01.12.
+//  Copyright (c) 2012 The Soulmen. All rights reserved.
+//
+
+#import "RKDefinitionsCollector.h"
+
+@implementation RKDefinitionsCollector
+
+@end
---
+++ Tests/RKAttributeCollectorTest.m
@@ -0,0 +1,19 @@
+//
+//  RKDefinitionsCollectorTest.m
+//  RTFKit
+//
+//  Created by Friedrich Gräter on 23.01.12.
+//  Copyright (c) 2012 The Soulmen. All rights reserved.
+//
+
+#import "RKDefinitionsCollectorTest.h"
+
+@implementation RKDefinitionsCollectorTest
+
+// All code under test must be linked into the Unit Test bundle
+- (void)testMath
+{
+    STAssertTrue((1 + 1) == 2, @"Compiler isn't feeling well today :-(");
+}
+
+@end
