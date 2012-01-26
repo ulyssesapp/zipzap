b1c3b3a31012c68b789466c6a49ce624c36ce5de
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-26 18:43:49 +0100

    Generation of section attributes

--- Source/Export/RKSectionWriter.m
+++ Source/Export/RKSectionWriter.m
@@ -11,9 +11,86 @@
 #import "RKResourcePool.h"
 #import "RKSectionWriter.h"
 
+@interface RKSectionWriter ()
+
+/*!
+ @abstract Translates all section attributes to RTF command
+ */
++ (NSString *)sectionAttributesForSection:(RKSection *)section;
+
+/*!
+ @abstract Translates all headers of a section to RTF
+ */
++ (NSString *)headersForSection:(RKSection *)section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources;
+
+/*!
+ @abstract Translates all footers of a section to RTF
+ */
++ (NSString *)footersForSection:(RKSection *)section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources;
+
+/*!
+ @abstract Translates the content of a section to RTF
+ */
++ (NSString *)contentForSection:(RKSection *)section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources;
+
+@end
+
 @implementation RKSectionWriter
 
 + (NSString *)RTFFromSection:(RKSection *)section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources
+{
+    NSString *sectionAttributes = [RKSectionWriter sectionAttributesForSection:section];
+
+    NSString *headers = [RKSectionWriter headersForSection:section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources];
+    NSString *footers = [RKSectionWriter footersForSection:section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources];
+    NSString *content = [RKSectionWriter contentForSection:section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources];
+    
+    return [NSString stringWithFormat:@"\n%@\n%@\n%@\n%@", sectionAttributes, headers, footers, content];
+}
+
++ (NSString *)sectionAttributesForSection:(RKSection *)section
+{
+    NSMutableString *attributes = [NSMutableString new];
+    
+    [attributes appendFormat: @"\\cols%u", section.numberOfColumns];
+    [attributes appendFormat: @"\\pgnstarts%u", section.indexOfFirstPage];
+    
+    switch (section.pageNumberingStyle) {
+        case RKPageNumberingDecimal:
+            [attributes appendFormat: @"\\pgndec"];
+            break;
+
+        case RKPageNumberingRomanLowerCase:
+            [attributes appendFormat: @"\\pgnlcrm"];
+            break;
+
+        case RKPageNumberingRomanUpperCase:
+            [attributes appendFormat: @"\\pgnucrm"];
+            break;
+
+        case RKPageNumberingAlphabeticUpperCase:
+            [attributes appendFormat: @"\\pgnultr"];
+            break;
+
+        case RKPageNumberingAlphabeticLowerCase:
+            [attributes appendFormat: @"\\pgnlltr"];
+            break;
+    }
+
+    return attributes;
+}
+     
++ (NSString *)headersForSection:(RKSection *)section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources
+{
+    return @"";
+}
+
++ (NSString *)footersForSection:(RKSection *)section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources
+{
+    return @"";
+}
+
++ (NSString *)contentForSection:(RKSection *)section withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources
 {
     return @"";
 }
--- Tests/Export/RKSectionWriterTest.m
+++ Tests/Export/RKSectionWriterTest.m
@@ -6,8 +6,39 @@
 //  Copyright (c) 2012 The Soulmen. All rights reserved.
 //
 
+#import "RKSection.h"
 #import "RKSectionWriterTest.h"
+#import "RKSectionWriter+TestExtensions.h"
 
 @implementation RKSectionWriterTest
+
+- (void)testGeneratingSectionHeaders
+{
+    RKSection *section = [RKSection sectionWithContent:[[NSAttributedString alloc] initWithString:@""]];
+
+    // Settings used in all tests
+    section.numberOfColumns = 2;
+    section.indexOfFirstPage = 3;
+    
+    // Decimal page numbering
+    section.pageNumberingStyle = RKPageNumberingDecimal;
+    STAssertEqualObjects([RKSectionWriter sectionAttributesForSection:section], @"\\cols2\\pgnstarts3\\pgndec", @"Invalid translation");
+
+    // Roman lower case
+    section.pageNumberingStyle = RKPageNumberingRomanLowerCase;
+    STAssertEqualObjects([RKSectionWriter sectionAttributesForSection:section], @"\\cols2\\pgnstarts3\\pgnlcrm", @"Invalid translation");
+
+    // Roman upper case
+    section.pageNumberingStyle = RKPageNumberingRomanUpperCase;
+    STAssertEqualObjects([RKSectionWriter sectionAttributesForSection:section], @"\\cols2\\pgnstarts3\\pgnucrm", @"Invalid translation");
+
+    // Letter upper case
+    section.pageNumberingStyle = RKPageNumberingAlphabeticUpperCase;
+    STAssertEqualObjects([RKSectionWriter sectionAttributesForSection:section], @"\\cols2\\pgnstarts3\\pgnultr", @"Invalid translation");
+
+    // Letter lower case
+    section.pageNumberingStyle = RKPageNumberingAlphabeticLowerCase;
+    STAssertEqualObjects([RKSectionWriter sectionAttributesForSection:section], @"\\cols2\\pgnstarts3\\pgnlltr", @"Invalid translation");
+}
 
 @end
