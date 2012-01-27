796438bfaec55125a8befe9271f95d4598eedb95
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-27 18:26:43 +0100

    Test extension for font style writer

---
+++ Tests/Utils/RKFontStyleWriter+TestExtensions.h
@@ -0,0 +1,13 @@
+//
+//  RKFontStyleWriter+TestExtensions.h
+//  RTFKit
+//
+//  Created by Friedrich Gräter on 27.01.12.
+//  Copyright (c) 2012 The Soulmen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol RKFontStyleWriter_TestExtensions <NSObject>
+
+@end
