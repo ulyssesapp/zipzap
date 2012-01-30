9db57358d5d9c0004f5f1778abf608d76c86424d
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-30 18:54:53 +0100

    Link support

--- Source/Export/Attribute Exporters/RKLinkWriter.m
+++ Source/Export/Attribute Exporters/RKLinkWriter.m
@@ -6,13 +6,42 @@
 //  Copyright (c) 2012 The Soulmen. All rights reserved.
 //
 
+#import "RKTaggedString.h"
 #import "RKLinkWriter.h"
+
+@interface RKLinkWriter ()
+
+/*!
+ @abstract Generates the required tag of a link attribute
+ */
++ (void)tag:(RKTaggedString *)taggedString withLink:(NSURL *)url inRange:(NSRange)range;
+
+@end
 
 @implementation RKLinkWriter
 
-+ (void)tag:(RKTaggedString *)taggedString withLinkStylesOfAttributedString:(NSAttributedString *)attributedString
++ (void)tag:(RKTaggedString *)taggedString withLink:(NSURL *)url inRange:(NSRange)range
 {
+    if (!url)
+        return;
     
+    [taggedString associateTag:[NSString stringWithFormat:@"{\\field{\\*\\fldinst{HYPERLINK \"%@\"}}{\\fldrslt ", [url absoluteString]]
+                    atPosition:range.location];
+
+    [taggedString associateTag:@"}}" atPosition:(range.location + range.length)];
 }
 
++ (void)tag:(RKTaggedString *)taggedString withLinkStylesOfAttributedString:(NSAttributedString *)attributedString
+{
+    [attributedString enumerateAttribute:NSLinkAttributeName inRange:NSMakeRange(0, [attributedString length]) options:0 usingBlock:^(id value, NSRange range, BOOL *stop) {
+        if ([value isKindOfClass: [NSString class]]) {
+            value = [NSURL URLWithString:value];
+        }
+        
+        [RKLinkWriter tag:taggedString withLink:value inRange:range];
+    }];
+}
+
+                                                                                 
+                                                                                 
 @end
--- Tests/Export/Attribute Exporters/RKLinkWriterTest.m
+++ Tests/Export/Attribute Exporters/RKLinkWriterTest.m
@@ -11,5 +11,22 @@
 
 @implementation RKLinkWriterTest
 
+- (void)testLinkWriter
+{
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:@"abcd"];
+    RKTaggedString *taggedString = [RKTaggedString taggedStringWithString:[attributedString string]];
+    
+    [attributedString addAttribute:NSLinkAttributeName value:[NSURL URLWithString:@"http://the-soulmen.com/"] range:NSMakeRange(1, 1)];
+    [attributedString addAttribute:NSLinkAttributeName value:[NSURL URLWithString:@"http://example.com/"] range:NSMakeRange(2, 1)];
+    
+    [RKLinkWriter tag:taggedString withLinkStylesOfAttributedString:attributedString];
+
+    STAssertEqualObjects([taggedString flattenedRTFString], 
+                         @"a"
+                          "{\\field{\\*\\fldinst{HYPERLINK \"http://the-soulmen.com/\"}}{\\fldrslt b}}"
+                          "{\\field{\\*\\fldinst{HYPERLINK \"http://example.com/\"}}{\\fldrslt c}}"
+                          "d",
+                         @"Invalid links created");
+}
 
 @end
