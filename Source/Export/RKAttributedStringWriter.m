41cac30ae41be6b76d697956a3cb86f4244ac760
Friedrich Gräter <friedrich@the-soulmen.com>
2012-01-31 19:31:56 +0100

    Using mutable dictionary

--- Source/Export/RKAttributedStringWriter.m
+++ Source/Export/RKAttributedStringWriter.m
@@ -13,47 +13,33 @@
 
 @interface RKAttributedStringWriter ()
 
-/*!
- @abstract Converts an attributed string to a tagged string
- */
-+ (RKTaggedString *)taggedStringFromAttributedString:(NSAttributedString *)attributedString withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources;
-
 @end
 
 @implementation RKAttributedStringWriter
 
-NSDictionary *attributeHandlers;
+NSMutableDictionary *attributeHandlers;
 
 + (void)registerHandler:(Class)attributeWriter forAttribute:(NSString*)attributeName
 {
     if (attributeHandlers == nil)
-        attributeHandlers = [NSDictionary new];
+        attributeHandlers = [NSMutableDictionary new];
     
     [attributeHandlers setValue:attributeWriter forKey:attributeName];
         
-    NSAssert([attributeWriter isSubclassOfClass: [RKAttributeWriter class]], @"Invalid attribute writer registered");
+    //NSAssert([attributeWriter isSubclassOfClass: [RKAttributeWriter class]], @"Invalid attribute writer registered");
 }
 
 + (NSString *)RTFfromAttributedString:(NSAttributedString *)attributedString withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources
 {
-    RKTaggedString *taggedString = [self taggedStringFromAttributedString:attributedString withAttachmentPolicy:attachmentPolicy resources:resources];
-    NSString *attachmentCharracterString = [NSString stringWithFormat:@"%C", NSAttachmentCharacter];
-    
-    // Return the tagged string and remove all useless attachment charracters
-    return [[taggedString flattenedRTFString] stringByReplacingOccurrencesOfString:attachmentCharracterString withString:@"" ];
-}
-
-+ (RKTaggedString *)taggedStringFromAttributedString:(NSAttributedString *)attributedString withAttachmentPolicy:(RKAttachmentPolicy)attachmentPolicy resources:(RKResourcePool *)resources
-{
     RKTaggedString *taggedString = [RKTaggedString taggedStringWithString:[attributedString string]];
-
+    
     [attributeHandlers enumerateKeysAndObjectsUsingBlock:^(NSString *attributeName, id handler, BOOL *stop) {
-       [attributedString enumerateAttribute:attributeName inRange:NSMakeRange(0, [attributedString length]) options:0 usingBlock:^(Class value, NSRange range, BOOL *stop) {
-           [handler addTagsForAttribute:value toTaggedString:taggedString inRange:range withAttachmentPolicy:attachmentPolicy resources:resources];
-       }];
+        [attributedString enumerateAttribute:attributeName inRange:NSMakeRange(0, [attributedString length]) options:0 usingBlock:^(Class value, NSRange range, BOOL *stop) {
+            [handler addTagsForAttribute:value toTaggedString:taggedString inRange:range withAttachmentPolicy:attachmentPolicy resources:resources];
+        }];
     }];
     
-    return taggedString;
+    return [taggedString flattenedRTFString];
 }
 
 @end
