79077136f08d2cf0bdcee0f392d1e5911bc56ca1
Friedrich Gräter <friedrich@the-soulmen.com>
2012-02-03 17:13:39 +0100

    Adding RKTextList and RKTextListItem to public header

--- Source/RTFKit.h
+++ Source/RTFKit.h
@@ -5,7 +5,10 @@
 //  Created by Max Seelemann on 16.01.12.
 //  Copyright (c) 2012 The Soulmen. All rights reserved.
 //
+
 #import "RKTypes.h"
 #import "RKSection.h"
 #import "RKDocument.h"
+
 #import "RKTextList.h"
+#import "RKTextListItem.h"
