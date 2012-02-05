cba3d3cbdb5a0bbb4c7f08214ca1da2b5a8e3c66
Friedrich Gräter <friedrich@the-soulmen.com>
2012-02-05 17:23:34 +0100

    Introducing a test helper for Cocoa reference tests

--- Tests/Utils/RKCocoaIntegrationTestHelper.h
+++ Tests/Utils/RKCocoaIntegrationTestHelper.h
@@ -6,10 +6,13 @@
 //  Copyright (c) 2012 The Soulmen. All rights reserved.
 //
 
-//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
-
-#import <SenTestingKit/SenTestingKit.h>
-
 @interface RKCocoaIntegrationTestHelper : SenTestCase
+
+- (NSAttributedString *)convertAndRereadRTF:(NSAttributedString *)attributedString documentAttributes:(NSDictionary **)documentAttributes;
+
+/*!
+ @abstract Tests whether the RTF conversion of an attributed string is identically read by Cocoa with respect to a certain attribute.
+ */
+- (void)assertReadingOfAttributedString:(NSAttributedString *)attributedString onAttribute:(NSString *)attributeName;
 
 @end
--- Tests/Utils/RKCocoaIntegrationTestHelper.m
+++ Tests/Utils/RKCocoaIntegrationTestHelper.m
@@ -10,10 +10,56 @@
 
 @implementation RKCocoaIntegrationTestHelper
 
-// All code under test must be linked into the Unit Test bundle
-- (void)testMath
+- (NSDictionary *)collectAttributesFromAttributedString:(NSAttributedString *)attributedString withName:(NSString *)attributeName
 {
-    STAssertTrue((1 + 1) == 2, @"Compiler isn't feeling well today :-(");
+    NSMutableDictionary *attributesMap;
+    
+    [attributedString enumerateAttribute:attributeName inRange:NSMakeRange(0, attributedString.length) options:0 usingBlock:^(id attributeValue, NSRange range, BOOL *stop) {
+        NSDictionary *rangeDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                         [NSNumber numberWithUnsignedInteger: range.location], @"location", 
+                                         [NSNumber numberWithUnsignedInteger: range.length], @"length", 
+                                         nil
+                                         ];
+        if (attributeValue)        
+            [attributesMap setObject:attributeValue forKey:rangeDictionary];
+    }];
+    
+    return attributesMap;
+}
+
+- (NSAttributedString *)convertAndRereadRTF:(NSAttributedString *)attributedString documentAttributes:(NSDictionary **)documentAttributes
+{
+    RKDocument *document = [RKDocument documentWithAttributedString:attributedString];
+    NSData *rtf = [document RTF];
+    
+   return [[NSAttributedString alloc] initWithRTF:rtf documentAttributes:documentAttributes];
+}
+
+- (void)assertReadingOfAttributedString:(NSAttributedString *)attributedString onAttribute:(NSString *)attributeName
+{
+    NSAttributedString *converted = [self convertAndRereadRTF:attributedString documentAttributes:NULL];
+    
+    // Collect attributes
+    NSDictionary *attributesMap = [self collectAttributesFromAttributedString:attributedString withName:attributeName];
+    NSDictionary *convertedAttributesMap = [self collectAttributesFromAttributedString:converted withName:attributeName];    
+
+    // Same count?
+    STAssertEquals(attributesMap.count, convertedAttributesMap.count, @"Different count of attributes");
+    
+    // Compare attributes
+    [convertedAttributesMap enumerateKeysAndObjectsUsingBlock:^(NSDictionary *key, id convertedAttributeValue, BOOL *stop) {
+        id originalAttributeValue = [attributesMap objectForKey: key];
+      
+        if ([convertedAttributeValue isKindOfClass:[NSColor class]] && [originalAttributeValue isKindOfClass:[NSColor class]]) {
+            NSColor *convertedColor = [NSColor rtfColorFromColor: convertedAttributeValue];
+            NSColor *originalColor = [NSColor rtfColorFromColor: originalAttributeValue];
+            
+            STAssertEqualObjects(convertedColor, originalColor, @"Attributes differ");
+        }
+         else {        
+            STAssertEqualObjects(originalAttributeValue, convertedAttributeValue, @"Attributes differ");
+        }
+    }];
 }
 
 @end
