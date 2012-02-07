e287ce0512c92f9c1f202948012b9318aa897509
Friedrich Gräter <friedrich@the-soulmen.com>
2012-02-07 15:25:01 +0100

    Factoring out reference test helper

---
+++ Tests/Utils/RKReferenceTest.h
@@ -0,0 +1,37 @@
+//
+//  RKReferenceTest.h
+//  RTFKit
+//
+//  Created by Friedrich Gräter on 07.02.12.
+//  Copyright (c) 2012 The Soulmen. All rights reserved.
+//
+
+/*!
+ @abstract Used for external reference tests 
+ @discussion External reference tests should verify whether the generated output is compatible 
+ with RTF files in the TestData/ directory. These RTF files are in turn used to manually verify
+ the compaitiblity of several features with other RTF readers such as MS Word, Nissus, Mellel, OpenOffice.org
+ */ 
+@interface SenTestCase (RKReferenceTest)
+
+/*!
+ @abstract Loads a document from the test bundle
+ */
+-(NSString *)loadTestDocument:(NSString *)name;
+
+/*!
+ @abstract Creates a variant of a string that is free of newlines and tabs
+ */
+- (NSString *)normalizeRTFString:(NSString *)rtf;
+
+/*!
+ @abstract Compares the content of two RTF files. Newlines and tabs are ignored
+ */
+- (void)assertGeneratedRTFString:(NSString *)stringA withExpectedString:(NSString *)stringB;
+
+/*!
+ @abstract Loads a test document from TestData and compares it charwise with the given RTF output. Ignores newlines and tabs.
+ */
+- (void)assertRTF:(NSData *)rtf withTestDocument:(NSString *)name;
+
+@end
--- Tests/Writer/RKWriterTest.m
+++ Tests/Writer/RKWriterTest.m
@@ -8,75 +8,7 @@
 
 #import "RKWriterTest.h"
 
-@interface RKWriterTest()
-
-/*!
- @abstract Loads a document from the test bundle
- */
--(NSString *)loadTestDocument:(NSString *)name;
-
-/*!
- @abstract Creates a variant of a string that is free of newlines and tabs
- */
-- (NSString *)normalizeRTFString:(NSString *)rtf;
-
-/*!
- @abstract Compares the content of two RTF files. Newlines and tabs are ignored
- */
-- (void)assertGeneratedRTFString:(NSString *)stringA withExpectedString:(NSString *)stringB;
-
-/*!
- @abstract Loads a test document from TestData and compares it charwise with the given RTF output. Ignores newlines and tabs.
- */
-- (void)assertRTF:(NSData *)rtf withTestDocument:(NSString *)name;
-
-@end
-
 @implementation RKWriterTest
-
-#pragma mark - Test helper
-
--(NSString *)loadTestDocument:(NSString *)name
-{
-	NSURL *url = [[NSBundle bundleForClass: [self class]] URLForResource:name withExtension:@"rtf" subdirectory:@"Test Data"];
-    NSString *content;
-    
-    STAssertNotNil(url, @"Cannot build URL");
-    
-	NSError *error;
-    content = [NSString stringWithContentsOfURL:url usedEncoding:NULL error:&error];
-	STAssertNotNil(content, @"Load failed with error: %@", error);
-    
-    return content;
-}
-
-- (NSString *)normalizeRTFString:(NSString *)rtf
-{
-    NSString *noNewlines = [rtf stringByReplacingOccurrencesOfString:@"\n" withString:@" " options:NSRegularExpressionSearch range:NSMakeRange(0, [rtf length])];
-    NSString *noLinefeeds = [noNewlines stringByReplacingOccurrencesOfString:@"\r" withString:@" " options:NSRegularExpressionSearch range:NSMakeRange(0, [rtf length])];
-    NSString *noTabs = [noLinefeeds stringByReplacingOccurrencesOfString:@"\t" withString:@" " options:NSRegularExpressionSearch range:NSMakeRange(0, [rtf length])];
-    NSString *noMultiSpaces = [noTabs stringByReplacingOccurrencesOfString:@"  " withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, [rtf length])];
-    
-    return noMultiSpaces;
-}
-
-- (void)assertGeneratedRTFString:(NSString *)generated withExpectedString:(NSString *)expected
-{
-    NSString *normalizedGenerated = [self normalizeRTFString: generated];
-    NSString *normalizedExpected = [self normalizeRTFString: expected];
-    
-    STAssertEqualObjects(normalizedGenerated, normalizedExpected, @"Unexpected RTF conversion.", normalizedGenerated, normalizedExpected);
-}
-
-- (void)assertRTF:(NSData *)rtf withTestDocument:(NSString *)name
-{
-    NSString *rtfContent = [[NSString alloc] initWithData:rtf encoding:NSASCIIStringEncoding];
-    NSString *testContent = [self loadTestDocument: name];
-
-    return [self assertGeneratedRTFString:rtfContent withExpectedString:testContent];
-}
-
-#pragma mark -
 
 - (void)testGeneratingEmptyRTFDocument
 {
@@ -94,6 +26,7 @@
     [self assertRTF: converted withTestDocument: @"simple"];
 }
 
+@end
 #pragma mark - Integration tests
 
 - (void)testGeneratingSimpleRTFD
@@ -111,4 +44,3 @@
     STAssertEqualObjects(convertedAttachment.fileWrapper.regularFileContents, image.fileWrapper.regularFileContents, @"File contents differ");
 }
 
-@end
