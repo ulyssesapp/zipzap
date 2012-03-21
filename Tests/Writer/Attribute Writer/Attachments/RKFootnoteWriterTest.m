0de01beb6140d477a5e39ca485220d502805a70e
Friedrich Gräter <friedrich@the-soulmen.com>
2012-03-21 15:27:47 +0100

    Adaption to new element ordering and constants

--- Tests/Writer/Attribute Writer/Attachments/RKFootnoteWriterTest.m
+++ Tests/Writer/Attribute Writer/Attachments/RKFootnoteWriterTest.m
@@ -16,22 +16,22 @@
     RKResourcePool *resources = [RKResourcePool new];
     NSMutableAttributedString *footnote = [[NSMutableAttributedString alloc] initWithString:@"aaa"];
     
-    [footnote addAttribute:NSFontAttributeName value:[NSFont fontWithName:@"Menlo" size:16] range:NSMakeRange(0, 3)];
+    [footnote addAttribute:RKFontAttributeName value:(__bridge_transfer id)CTFontCreateWithName((__bridge CFStringRef)@"GillSans", 16, NULL) range:NSMakeRange(0, 3)];
     
-    NSString *string = [NSString stringWithFormat:@">%C<", NSAttachmentCharacter];
+    NSString *string = [NSString stringWithFormat:@">%C<", RKAttachmentCharacter];
     RKTaggedString *taggedString = [RKTaggedString taggedStringWithString:string];
     
     [RKFootnoteWriter addTagsForAttribute:RKFootnoteAttributeName value:footnote effectiveRange:NSMakeRange(1,1) toString:taggedString originalString:nil attachmentPolicy:0 resources:resources];
     
     // Valid string tagging
     STAssertEqualObjects([taggedString flattenedRTFString],
-                         @">{\\super \\chftn }{\\footnote {\\super \\chftn } \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs32\\fsmilli16000 \\strikec0 \\strokec0 \\ulc0 aaa\\par\n}<",
+                         @">{\\super \\chftn }{\\footnote {\\super \\chftn } \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs32\\fsmilli16000 aaa\\par\n}<",
                          @"Invalid footnote generated"
                          );
     
     // Font was collected
     STAssertEquals(resources.fontFamilyNames.count, (NSUInteger)1, @"Invalid count of fonts");
-    STAssertEqualObjects([resources.fontFamilyNames objectAtIndex:0], @"Menlo-Regular", @"Missing font");    
+    STAssertEqualObjects([resources.fontFamilyNames objectAtIndex:0], @"GillSans", @"Missing font");    
 }
 
 - (void)testGenerateEndnote
@@ -39,29 +39,30 @@
     RKResourcePool *resources = [RKResourcePool new];
     NSMutableAttributedString *footnote = [[NSMutableAttributedString alloc] initWithString:@"aaa"];
     
-    [footnote addAttribute:NSFontAttributeName value:[NSFont fontWithName:@"Menlo" size:16] range:NSMakeRange(0, 3)];
+    [footnote addAttribute:RKFontAttributeName value:(__bridge_transfer id)CTFontCreateWithName((__bridge CFStringRef)@"GillSans", 16, NULL) range:NSMakeRange(0, 3)];
     
-    NSString *string = [NSString stringWithFormat:@">%C<", NSAttachmentCharacter];
+    NSString *string = [NSString stringWithFormat:@">%C<", RKAttachmentCharacter];
     RKTaggedString *taggedString = [RKTaggedString taggedStringWithString:string];
     
     [RKFootnoteWriter addTagsForAttribute:RKEndnoteAttributeName value:footnote effectiveRange:NSMakeRange(1,1) toString:taggedString originalString:nil attachmentPolicy:0 resources:resources];
     
     // Valid string tagging
     STAssertEqualObjects([taggedString flattenedRTFString],
-                         @">{\\super \\chftn }{\\footnote\\ftnalt {\\super \\chftn } \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs32\\fsmilli16000 \\strikec0 \\strokec0 \\ulc0 aaa\\par\n}<",
+                         @">{\\super \\chftn }{\\footnote\\ftnalt {\\super \\chftn } \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs32\\fsmilli16000 aaa\\par\n}<",
                          @"Invalid footnote generated"
-                         );
+                          );
     
     // Font was collected
     STAssertEquals(resources.fontFamilyNames.count, (NSUInteger)1, @"Invalid count of fonts");
-    STAssertEqualObjects([resources.fontFamilyNames objectAtIndex:0], @"Menlo-Regular", @"Missing font");    
+    STAssertEqualObjects([resources.fontFamilyNames objectAtIndex:0], @"GillSans", @"Missing font");    
 }
 
+#if !TARGET_OS_IPHONE
 - (void)testFootnotesAreIgnoredByCocoa
 {
     NSMutableAttributedString *footnote = [[NSMutableAttributedString alloc] initWithString:@"aaa"];    
     
-    NSMutableAttributedString *original = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"a%Cbc", NSAttachmentCharacter]];
+    NSMutableAttributedString *original = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"a%Cbc", RKAttachmentCharacter]];
     
     [original addAttribute:RKFootnoteAttributeName value:footnote range:NSMakeRange(1, 1)];
     
@@ -74,15 +75,16 @@
     
     STAssertEqualObjects([converted string], @"abc\n", @"Invalid string content");    
 }
+#endif
 
 - (void)testFootnotesAreCompatibleToManualReferenceTest
 {
     // Footnote with some inline formatting
     NSMutableAttributedString *footnote = [[NSMutableAttributedString alloc] initWithString:@"aaa"];    
-    [footnote addAttribute:NSFontAttributeName value:[NSFont fontWithName:@"Helvetica-Bold" size:12] range:NSMakeRange(1,1)];
+    [footnote addAttribute:RKFontAttributeName value:(__bridge_transfer id)CTFontCreateWithName((__bridge CFStringRef)@"Helvetica-Bold", 12, NULL) range:NSMakeRange(1,1)];
     
     // Text with an inline footnote
-    NSMutableAttributedString *original = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"a%Cbc", NSAttachmentCharacter]];
+    NSMutableAttributedString *original = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"a%Cbc", RKAttachmentCharacter]];
     
     [original addAttribute:RKFootnoteAttributeName value:footnote range:NSMakeRange(1, 1)];
 
@@ -97,10 +99,10 @@
 {
     // Footnote with some inline formatting
     NSMutableAttributedString *endnote = [[NSMutableAttributedString alloc] initWithString:@"aaa"];    
-    [endnote addAttribute:NSFontAttributeName value:[NSFont fontWithName:@"Helvetica-Bold" size:12] range:NSMakeRange(1,1)];
+    [endnote addAttribute:RKFontAttributeName value:(__bridge_transfer id)CTFontCreateWithName((__bridge CFStringRef)@"Helvetica-Bold", 12, NULL) range:NSMakeRange(1,1)];
     
     // Text with an inline footnote
-    NSMutableAttributedString *original = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"a%Cbc", NSAttachmentCharacter]];
+    NSMutableAttributedString *original = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"a%Cbc", RKAttachmentCharacter]];
     
     [original addAttribute:RKEndnoteAttributeName value:endnote range:NSMakeRange(1, 1)];
     
--- Tests/Writer/Structure Writer/RKSectionWriterTest.m
+++ Tests/Writer/Structure Writer/RKSectionWriterTest.m
@@ -49,7 +49,7 @@
     [section setHeader:[[NSAttributedString alloc] initWithString:@"Text"] forPages:RKPageSelectionLeft];
     
     STAssertEqualObjects([RKSectionWriter headersForSection:section withAttachmentPolicy:0 resources:resources],
-                         @"{\\headerl \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs24\\fsmilli12000 \\strikec0 \\strokec0 \\ulc0 Text\\par\n}",
+                         @"{\\headerl \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs24\\fsmilli12000 Text\\par\n}",
                          @"Invalid header generated"
                          );
 
@@ -57,7 +57,7 @@
     [section setHeader:[[NSAttributedString alloc] initWithString:@"Text"] forPages:RKPageSelectionRight];
     
     STAssertEqualObjects([RKSectionWriter headersForSection:section withAttachmentPolicy:0 resources:resources],
-                         @"{\\headerr \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs24\\fsmilli12000 \\strikec0 \\strokec0 \\ulc0 Text\\par\n}",
+                         @"{\\headerr \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs24\\fsmilli12000 Text\\par\n}",
                          @"Invalid header generated"
                          );
 
@@ -65,7 +65,7 @@
     [section setHeader:[[NSAttributedString alloc] initWithString:@"Text"] forPages:RKPageSelectionFirst];
     
     STAssertEqualObjects([RKSectionWriter headersForSection:section withAttachmentPolicy:0 resources:resources],
-                         @"{\\headerf \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs24\\fsmilli12000 \\strikec0 \\strokec0 \\ulc0 Text\\par\n}",
+                         @"{\\headerf \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs24\\fsmilli12000 Text\\par\n}",
                          @"Invalid header generated"
                          );    
 }
@@ -78,7 +78,7 @@
     [section setFooter:[[NSAttributedString alloc] initWithString:@"Text"] forPages:RKPageSelectionLeft];
     
     STAssertEqualObjects([RKSectionWriter footersForSection:section withAttachmentPolicy:0 resources:resources],
-                         @"{\\footerl \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs24\\fsmilli12000 \\strikec0 \\strokec0 \\ulc0 Text\\par\n}",
+                         @"{\\footerl \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs24\\fsmilli12000 Text\\par\n}",
                          @"Invalid footer generated"
                          );
     
@@ -86,7 +86,7 @@
     [section setFooter:[[NSAttributedString alloc] initWithString:@"Text"] forPages:RKPageSelectionRight];
     
     STAssertEqualObjects([RKSectionWriter footersForSection:section withAttachmentPolicy:0 resources:resources],
-                         @"{\\footerr \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs24\\fsmilli12000 \\strikec0 \\strokec0 \\ulc0 Text\\par\n}",
+                         @"{\\footerr \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs24\\fsmilli12000 Text\\par\n}",
                          @"Invalid footer generated"
                          );
     
@@ -94,7 +94,7 @@
     [section setFooter:[[NSAttributedString alloc] initWithString:@"Text"] forPages:RKPageSelectionFirst];
     
     STAssertEqualObjects([RKSectionWriter footersForSection:section withAttachmentPolicy:0 resources:resources],
-                         @"{\\footerf \\pard\\ql\\pardeftab0 \\cb1 \\cf0 \\f0 \\fs24\\fsmilli12000 \\strikec0 \\strokec0 \\ulc0 Text\\par\n}",
+                         @"{\\footerf \\pard \\cb1 \\cf0 \\strikec0 \\strokec0 \\f0 \\fs24\\fsmilli12000 Text\\par\n}",
                          @"Invalid footer generated"
                          );    
 }
@@ -156,7 +156,7 @@
     NSMutableAttributedString *content = [[NSMutableAttributedString alloc] 
                                            initWithString:[NSString stringWithFormat:
                                                            @"%C\f%C\f%C",
-                                                           NSAttachmentCharacter
+                                                           RKAttachmentCharacter
                                                            ]
                                            ];
 
