fde6fbaf0f999c05d490ccaa9ddd51ad8e73663d
Friedrich Gräter <friedrich@the-soulmen.com>
2012-07-21 14:10:09 +0200

    Portable version of RKListItemWriter

--- Source/Writer/Attribute Writer/Paragraph Styles/RKListItemWriter.m
+++ Source/Writer/Attribute Writer/Paragraph Styles/RKListItemWriter.m
@@ -11,6 +11,22 @@
 #import "RKListItem.h"
 #import "RKListStyle+WriterAdditions.h"
 #import "RKConversion.h"
+#import "RKParagraphStyleWrapper.h"
+#import "RKTextTabWrapper.h"
+
+@interface RKListItemWriter ()
+
+/*!
+ @abstract Updates the paragraph style of an attributed string at a certain range according to the given tab stop and indentation settings.
+ */
++ (void)updateParagraphStyleOfAttributedString:(NSMutableAttributedString *)attributedString inRange:(NSRange)range usingTabAlignments:(NSArray *)tabAlignments tabLocations:(NSArray *)tabLocations firstLineHeadIndentOfset:(CGFloat)firstLineHeadIndentOffset headIndentOffset:(CGFloat)headIndentOffset;
+
+/*!
+ @abstract Updates the given paragraph style according to the given tab stop and indentation settings.
+*/
++ (void)updateParagraphStyle:(RKParagraphStyleWrapper *)paragraphStyle usingTabAlignments:(NSArray *)tabAlignments tabLocations:(NSArray *)tabLocations firstLineHeadIndentOfset:(CGFloat)firstLineHeadIndentOffset headIndentOffset:(CGFloat)headIndentOffset;
+
+@end
 
 @implementation RKListItemWriter
 
@@ -36,44 +52,98 @@
     if (!listStyle.firstLineHeadIndentOffsets && !listStyle.headIndentOffsets && !listStyle.tabStopLocations)
         return;
 
-    // Get paragraph style
-    NSMutableParagraphStyle *paragraphStyle = [[preprocessedString attribute:NSParagraphStyleAttributeName atIndex:range.location effectiveRange:NULL] mutableCopy];
-    
-    if (!paragraphStyle)
-        paragraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-
     // Get indentation
-    CGFloat firstLineHeadIndentOffset = [listStyle.firstLineHeadIndentOffsets[listItem.indentationLevel] floatValue];
-    CGFloat headIndentOffset = [listStyle.headIndentOffsets[listItem.indentationLevel] floatValue];
+    CGFloat firstLineHeadIndentOffset = 0;
+    CGFloat headIndentOffset = 0;
     
-    // Add indentation according to indentation level of a list item
     if (listStyle.firstLineHeadIndentOffsets.count > listItem.indentationLevel)
-        paragraphStyle.firstLineHeadIndent += firstLineHeadIndentOffset;
+        firstLineHeadIndentOffset = [[listStyle.firstLineHeadIndentOffsets objectAtIndex: listItem.indentationLevel] floatValue];
     
     if (listStyle.headIndentOffsets.count > listItem.indentationLevel)
-        paragraphStyle.headIndent += headIndentOffset;
+        headIndentOffset = [[listStyle.headIndentOffsets objectAtIndex: listItem.indentationLevel] floatValue];
 
     // Adjust tab stops
+    NSArray *tabLocations = nil;
+    NSArray *tabAlignments = nil;
+    
     if ((listStyle.tabStopLocations.count > listItem.indentationLevel) && (listStyle.tabStopAlignments.count > listItem.indentationLevel)) {
-        NSArray *tabStopLocations = listStyle.tabStopLocations[listItem.indentationLevel];
-        NSArray *tabStopAlignments = listStyle.tabStopAlignments[listItem.indentationLevel];
-
-        // Setup new NSTextTabs instances for the given tabs stops
-        NSMutableArray *newTabStops = [NSMutableArray new];
-        
-        for (NSUInteger tabIndex = 0; tabIndex < tabStopLocations.count; tabIndex ++) {
-            CGFloat location = [tabStopLocations[tabIndex] floatValue] + firstLineHeadIndentOffset;
-            
-            NSTextTab *newTab = [[NSTextTab alloc] initWithTextAlignment:[tabStopAlignments[tabIndex] unsignedIntegerValue] location:location options:nil];
-            [newTabStops addObject: newTab];
-        }
-        
-        paragraphStyle.tabStops = newTabStops;
+        tabLocations = listStyle.tabStopLocations[listItem.indentationLevel];
+        tabAlignments = listStyle.tabStopAlignments[listItem.indentationLevel];
     }
         
     // Update paragraph style
-    [preprocessedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:range];
+    [self updateParagraphStyleOfAttributedString:preprocessedString inRange:range usingTabAlignments:tabAlignments tabLocations:tabLocations firstLineHeadIndentOfset:firstLineHeadIndentOffset headIndentOffset:headIndentOffset];
 }
+
+#if !TARGET_OS_IPHONE
++ (void)updateParagraphStyleOfAttributedString:(NSMutableAttributedString *)attributedString inRange:(NSRange)range usingTabAlignments:(NSArray *)tabAlignments tabLocations:(NSArray *)tabLocations firstLineHeadIndentOfset:(CGFloat)firstLineHeadIndentOffset headIndentOffset:(CGFloat)headIndentOffset
+{
+    // Get paragraph style
+    NSMutableParagraphStyle *nsParagraphStyle = [[attributedString attribute:NSParagraphStyleAttributeName atIndex:range.location effectiveRange:NULL] mutableCopy];
+    if (!nsParagraphStyle)
+        nsParagraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+
+    // Update paragraph style
+    RKParagraphStyleWrapper *paragraphStyle = [[RKParagraphStyleWrapper alloc] initWithNSParagraphStyle: nsParagraphStyle];
+    
+    [self updateParagraphStyle:paragraphStyle usingTabAlignments:tabAlignments tabLocations:tabLocations firstLineHeadIndentOfset:firstLineHeadIndentOffset headIndentOffset:headIndentOffset];
+    
+    // Update attribute
+    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle.newNSParagraphStyle range:range];
+}
+#else
+
++ (void)updateParagraphStyleOfAttributedString:(NSMutableAttributedString *)attributedString inRange:(NSRange)range usingTabAlignments:(NSArray *)tabAlignments tabLocations:(NSArray *)tabLocations firstLineHeadIndentOfset:(CGFloat)firstLineHeadIndentOffset headIndentOffset:(CGFloat)headIndentOffset
+{
+    RKParagraphStyleWrapper *paragraphStyle = nil;
+    
+    // Get paragraph style
+    CTParagraphStyleRef ctParagraphStyle = (__bridge CTParagraphStyleRef)[[attributedString attribute:NSParagraphStyleAttributeName atIndex:range.location effectiveRange:NULL] mutableCopy];
+    
+    // Use default paragraph style otherwise
+    if (!ctParagraphStyle) {
+        ctParagraphStyle = CTParagraphStyleCreate(NULL, 0);
+        paragraphStyle = [[RKParagraphStyleWrapper alloc] initWithCTParagraphStyle:ctParagraphStyle];
+        
+        CFRelease(ctParagraphStyle);
+    }
+    
+    paragraphStyle = [[RKParagraphStyleWrapper alloc] initWithCTParagraphStyle:ctParagraphStyle];
+    
+    // Update paragraph style
+    [self updateParagraphStyle:paragraphStyle usingTabAlignments:tabAlignments tabLocations:tabLocations firstLineHeadIndentOfset:firstLineHeadIndentOffset headIndentOffset:headIndentOffset];
+
+    CTParagraphStyleRef updatedStyle = paragraphStyle.newCTParagraphStyle;
+    [attributedString addAttribute:NSParagraphStyleAttributeName value:(__bridge id)updatedStyle range:range];
+    
+    CFRelease(updatedStyle);
+}
+
+#endif
+
++ (void)updateParagraphStyle:(RKParagraphStyleWrapper *)paragraphStyle usingTabAlignments:(NSArray *)tabAlignments tabLocations:(NSArray *)tabLocations firstLineHeadIndentOfset:(CGFloat)firstLineHeadIndentOffset headIndentOffset:(CGFloat)headIndentOffset
+{
+    // Update Indentation
+    paragraphStyle.firstLineHeadIndent += firstLineHeadIndentOffset;
+    paragraphStyle.headIndent += headIndentOffset;
+    
+    // Setup new NSTextTabs instances for the given tabs stops
+    NSMutableArray *newTabStops = [NSMutableArray new];
+    
+    for (NSUInteger tabIndex = 0; tabIndex < tabLocations.count; tabIndex ++) {
+        CGFloat location = [[tabLocations objectAtIndex: tabIndex] floatValue] + firstLineHeadIndentOffset;
+        NSUInteger alignment = tabAlignments.count > tabIndex ? [[tabAlignments objectAtIndex: tabIndex] unsignedIntegerValue] : 0;
+        
+        RKTextTabWrapper *newTab = [RKTextTabWrapper new];
+        newTab.tabAlignment = alignment;
+        newTab.location = location;
+        
+        [newTabStops addObject: newTab];
+    }
+    
+    paragraphStyle.tabStops = newTabStops;
+}
+
 
 + (void)addTagsForAttribute:(NSString *)attributeName
                       value:(RKListItem *)listItem
