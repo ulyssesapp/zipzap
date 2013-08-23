182c75915237899d9a4e050ccdb5bc2a768acc3a
Friedrich Gräter <friedrich@the-soulmen.com>
2013-08-23 11:31:14 +0200

    ULYSSES-3067 Setup section breaks according to page binding in RTF

--- Source/Writer/Structure Writer/RKSectionWriter.h
+++ Source/Writer/Structure Writer/RKSectionWriter.h
@@ -12,14 +12,29 @@
 @class RKResourcePool;
 
 /*!
+ @abstract The position of the first page.
+ 
+ @const RKSectionStartsOnSamePage		The section should start on the same page.
+ @const RKSectionStartsOnNextPage		The first page of the section should be placed on the next page.
+ @const RKSectionStartsOnOddPage		The first page of the section should be placed on an od page.
+ @const RKSectionStartsOnEvenPage		The first page of the section should be placed on an od page.
+ */
+typedef enum : NSUInteger {
+	RKSectionStartsOnSamePage,
+	RKSectionStartsOnNextPage,
+	RKSectionStartsOnOddPage,
+	RKSectionStartsOnEvenPage
+}RKSectionFirstPagePosition;
+
+/*!
  @abstract Translates the content of an RTF section
  */
 @interface RKSectionWriter : NSObject
 
 /*!
  @abstract Translates an RKSection to RTF
- @discussion Requires an attachment policy to specify how attached files are exported and a resource pool to collect fonts and colors.
+ @discussion Requires an attachment policy to specify how attached files are exported and a resource pool to collect fonts and colors. It can be specified on which position the first page of a section should be placed (left, right, next)
  */
-+ (NSString *)RTFFromSection:(RKSection *)section withConversionPolicy:(RKConversionPolicy)conversionPolicy resources:(RKResourcePool *)resources;
++ (NSString *)RTFFromSection:(RKSection *)section withConversionPolicy:(RKConversionPolicy)conversionPolicy firstPagePosition:(RKSectionFirstPagePosition)firstPagePosition resources:(RKResourcePool *)resources;
 
 @end
