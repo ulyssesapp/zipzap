3a82eb25f3228b64f9990fd3e1f01b2dd52158dd
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-01 18:05:07 +0200

    Extracted points-to-half-points conversion into own method

--- Source/DOCX/Content Writer/RKDOCXFontAttributeWriter.m
+++ Source/DOCX/Content Writer/RKDOCXFontAttributeWriter.m
@@ -32,10 +32,23 @@ NSString *RKDOCXFontAttributeFontSizePropertyName				= @"w:sz";
 																[NSXMLElement attributeWithName:RKDOCXFontAttributeEastAsiaFontName stringValue:[attributes[RKFontAttributeName] familyName]],
 																[NSXMLElement attributeWithName:RKDOCXFontAttributeHighAnsiFontName stringValue:[attributes[RKFontAttributeName] familyName]]
 																]];
-	NSXMLElement *sizeElement = [NSXMLElement elementWithName:RKDOCXFontAttributeFontSizePropertyName children:nil attributes:@[[NSXMLElement attributeWithName:@"w:val" stringValue:[NSString stringWithFormat: @"%0.0f", [attributes[RKFontAttributeName] pointSize] * 2]]]];
-	NSXMLElement *complexSizeElement = [NSXMLElement elementWithName:RKDOCXFontAttributeComplexScriptFontSizePropertyName children:nil attributes:@[[NSXMLElement attributeWithName:@"w:val" stringValue:[NSString stringWithFormat: @"%0.0f", [attributes[RKFontAttributeName] pointSize] * 2]]]];
+	
+	NSUInteger wordFontSize = [self wordFontSizeFromPointSize: [attributes[RKFontAttributeName] pointSize]];
+	NSString *fontSize = [@(wordFontSize) stringValue];
+	
+	NSXMLElement *sizeElement = [NSXMLElement elementWithName:RKDOCXFontAttributeFontSizePropertyName children:nil attributes:@[[NSXMLElement attributeWithName:@"w:val" stringValue: fontSize]]];
+	NSXMLElement *complexSizeElement = [NSXMLElement elementWithName:RKDOCXFontAttributeComplexScriptFontSizePropertyName children:nil attributes:@[[NSXMLElement attributeWithName:@"w:val" stringValue: fontSize]]];
 	
 	return @[fontElement, sizeElement, complexSizeElement];
+}
+
++ (NSUInteger)wordFontSizeFromPointSize:(CGFloat)pointSize
+{
+	// Font sizes are saved in half points, so the point size has to be doubled
+	CGFloat wordFontSize = pointSize * 2;
+	
+	// For now we round the font size
+	return (NSUInteger)round(wordFontSize);
 }
 
 @end
