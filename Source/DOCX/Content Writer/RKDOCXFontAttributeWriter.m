f57616128f73de6a099c39bd68bdff35de55af4b
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-01 17:50:23 +0200

    Implements all tested features

--- Source/DOCX/Content Writer/RKDOCXFontAttributeWriter.m
+++ Source/DOCX/Content Writer/RKDOCXFontAttributeWriter.m
@@ -8,7 +8,34 @@
 
 #import "RKDOCXFontAttributeWriter.h"
 
+NSString *RKDOCXFontAttributeAsciiFontName						= @"w:ascii";
+NSString *RKDOCXFontAttributeComplexScriptFontName				= @"w:cs";
+NSString *RKDOCXFontAttributeComplexScriptFontSizePropertyName	= @"w:szCs";
+NSString *RKDOCXFontAttributeEastAsiaFontName					= @"w:eastAsia";
+NSString *RKDOCXFontAttributeFontPropertyName					= @"w:rFonts";
+NSString *RKDOCXFontAttributeHighAnsiFontName					= @"w:hAnsi";
+NSString *RKDOCXFontAttributeFontSizePropertyName				= @"w:sz";
+
 
 @implementation RKDOCXFontAttributeWriter
+
++ (NSArray *)runPropertiesForAttributes:(NSDictionary *)attributes
+{
+	if (!attributes[RKFontAttributeName])
+		return nil;
+	
+	NSXMLElement *fontElement = [NSXMLElement elementWithName:RKDOCXFontAttributeFontPropertyName
+													 children:nil
+												   attributes:@[
+																[NSXMLElement attributeWithName:RKDOCXFontAttributeAsciiFontName stringValue:[attributes[RKFontAttributeName] familyName]],
+																[NSXMLElement attributeWithName:RKDOCXFontAttributeComplexScriptFontName stringValue:[attributes[RKFontAttributeName] familyName]],
+																[NSXMLElement attributeWithName:RKDOCXFontAttributeEastAsiaFontName stringValue:[attributes[RKFontAttributeName] familyName]],
+																[NSXMLElement attributeWithName:RKDOCXFontAttributeHighAnsiFontName stringValue:[attributes[RKFontAttributeName] familyName]]
+																]];
+	NSXMLElement *sizeElement = [NSXMLElement elementWithName:RKDOCXFontAttributeFontSizePropertyName children:nil attributes:@[[NSXMLElement attributeWithName:@"w:val" stringValue:[NSString stringWithFormat: @"%0.0f", [attributes[RKFontAttributeName] pointSize] * 2]]]];
+	NSXMLElement *complexSizeElement = [NSXMLElement elementWithName:RKDOCXFontAttributeComplexScriptFontSizePropertyName children:nil attributes:@[[NSXMLElement attributeWithName:@"w:val" stringValue:[NSString stringWithFormat: @"%0.0f", [attributes[RKFontAttributeName] pointSize] * 2]]]];
+	
+	return @[fontElement, sizeElement, complexSizeElement];
+}
 
 @end
--- Source/DOCX/Content Writer/RKDOCXRunAttributeWriter.m
+++ Source/DOCX/Content Writer/RKDOCXRunAttributeWriter.m
@@ -20,18 +20,33 @@ NSString *RKDOCXRunAttributeTextElementName				= @"w:t";
 
 + (NSXMLElement *)runElementForAttributedString:(NSAttributedString *)attributedString attributes:(NSDictionary *)attributes range:(NSRange)range
 {
+	// Check for empty range
+	if (!range.length)
+		return nil;
+	
 	// Check for invalid range
 	NSParameterAssert(NSMaxRange(range) <= attributedString.length);
 	
-//	NSMutableArray *properties = [NSMutableArray new];
-//	
-//	// Collect all matching attributes
-//	[properties addObjectsFromArray: [RKDOCXFontAttributeWriter runPropertiesForAttributes: attributes]];
-//	// ...
-//	
-//	NSXMLElement *runPropertiesElement = [NSXMLElement elementWithName:RKDOCXRunAttributeRunPropertiesElementName children:properties attributes:nil];
-//	
-	return nil;
+	NSMutableArray *properties = [NSMutableArray new];
+	
+	// Collect all matching attributes
+	NSArray *propertyElements = [RKDOCXFontAttributeWriter runPropertiesForAttributes: attributes];
+	if (propertyElements)
+		[properties addObjectsFromArray: propertyElements];
+	// ...
+	
+	NSXMLElement *runElement = [NSXMLElement elementWithName:RKDOCXRunAttributeRunElementName];
+	
+	if (properties.count) {
+		NSXMLElement *runPropertiesElement = [NSXMLElement elementWithName:RKDOCXRunAttributeRunPropertiesElementName children:properties attributes:nil];
+		[runElement addChild: runPropertiesElement];
+	}
+	
+	NSXMLElement *textElement = [NSXMLElement elementWithName:RKDOCXRunAttributeTextElementName stringValue:[attributedString.string substringWithRange:range]];
+	[textElement addAttribute: [NSXMLElement attributeWithName:@"xml:space" stringValue:@"preserve"]];
+	[runElement addChild: textElement];
+	
+	return runElement;
 }
 
 @end
