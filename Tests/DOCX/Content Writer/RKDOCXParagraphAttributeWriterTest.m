0937ce06dd54135faa0086161a469889a195f70b
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-09 17:59:21 +0200

    Adds test case for default tab interval

--- Tests/DOCX/Content Writer/RKDOCXParagraphAttributeWriterTest.m
+++ Tests/DOCX/Content Writer/RKDOCXParagraphAttributeWriterTest.m
@@ -136,4 +136,17 @@
 	[self assertDOCX:converted withTestDocument:@"tabstops"];
 }
 
+- (void)testParagraphElementWithDefaultTabInterval
+{
+	NSMutableParagraphStyle *paragraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+	paragraphStyle.defaultTabInterval = 8;
+	NSDictionary *attributes = @{RKParagraphStyleAttributeName: paragraphStyle};
+	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"Custom Tab Stop Test: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\tAt vero eos et accusam et justo duo dolores et ea rebum.\tStet clita kasd gubergren, no sea takimata sanctus est.\tLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\tAt vero eos et accusam et justo duo dolores et ea rebum.\tStet clita kasd gubergren, no sea takimata sanctus est.\tLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\tAt vero eos et accusam et justo duo dolores et ea rebum.\tStet clita kasd gubergren, no sea takimata sanctus est." attributes:attributes];
+	
+	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
+	NSData *converted = [document DOCX];
+	
+	[self assertDOCX:converted withTestDocument:@"defaulttabinterval"];
+}
+
 @end
