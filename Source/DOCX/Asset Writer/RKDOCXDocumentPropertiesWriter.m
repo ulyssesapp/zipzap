16af373e056b2ffc2e8273fd332943e86e296e3d
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-10 18:08:23 +0200

    Adds support for core properties

--- Source/DOCX/Asset Writer/RKDOCXDocumentPropertiesWriter.m
+++ Source/DOCX/Asset Writer/RKDOCXDocumentPropertiesWriter.m
@@ -9,8 +9,20 @@
 #import "RKDOCXDocumentPropertiesWriter.h"
 
 // Root element names
-NSString *RKDOCXCorePropertiesRootElementName		= @"cp:coreProperties";
-NSString *RKDOCXExtendedPropertiesRootElementName	= @"Properties";
+NSString *RKDOCXCorePropertiesRootElementName				= @"cp:coreProperties";
+NSString *RKDOCXExtendedPropertiesRootElementName			= @"Properties";
+
+// Property element names
+NSString *RKDOCXDocumentPropertiesAuthorPropertyName		= @"dc:creator";
+NSString *RKDOCXDocumentPropertiesCategoryPropertyName		= @"cp:category";
+NSString *RKDOCXDocumentPropertiesCreationTimePropertyName	= @"dcterms:created";
+NSString *RKDOCXDocumentPropertiesEditorPropertyName		= @"cp:lastModifiedBy";
+NSString *RKDOCXDocumentPropertiesKeywordsPropertyName		= @"cp:keywords";
+NSString *RKDOCXDocumentPropertiesModificationPropertyName	= @"dcterms:modified";
+NSString *RKDOCXDocumentPropertiesSubjectPropertyName		= @"dc:subject";
+NSString *RKDOCXDocumentPropertiesTimeAttributeName			= @"xsi:type";
+NSString *RKDOCXDocumentPropertiesTimeTypeName				= @"dcterms:W3CDTF";
+NSString *RKDOCXDocumentPropertiesTitlePropertyName			= @"dc:title";
 
 @implementation RKDOCXDocumentPropertiesWriter
 
@@ -27,6 +39,9 @@ NSString *RKDOCXExtendedPropertiesRootElementName	= @"Properties";
 	
 	NSXMLDocument *document = [self basicXMLDocumentWithRootElementName:RKDOCXCorePropertiesRootElementName namespaces:namespaces];
 	
+	// Core Properties
+	document.rootElement.children = [self corePropertyElementsFromContext: context];
+	
 	[context addDocumentPart:[document XMLDataWithOptions: NSXMLNodePrettyPrint | NSXMLNodeCompactEmptyElement] withFilename:RKDOCXCorePropertiesFilename];
 }
 
@@ -41,6 +56,58 @@ NSString *RKDOCXExtendedPropertiesRootElementName	= @"Properties";
 	NSXMLDocument *document = [self basicXMLDocumentWithRootElementName:RKDOCXExtendedPropertiesRootElementName namespaces:namespaces];
 	
 	[context addDocumentPart:[document XMLDataWithOptions: NSXMLNodePrettyPrint | NSXMLNodeCompactEmptyElement] withFilename:RKDOCXExtendedPropertiesFilename];
+}
+
++ (NSArray *)corePropertyElementsFromContext:(RKDOCXConversionContext *)context
+{
+	NSDictionary *metadata = context.document.metadata;
+	if (!metadata)
+		return nil;
+	
+	NSMutableArray *coreProperties = [NSMutableArray new];
+	
+	if (metadata[NSTitleDocumentAttribute])
+		[coreProperties addObject: [NSXMLElement elementWithName:RKDOCXDocumentPropertiesTitlePropertyName stringValue:metadata[NSTitleDocumentAttribute]]];
+	
+	if (metadata[NSSubjectDocumentAttribute])
+		[coreProperties addObject: [NSXMLElement elementWithName:RKDOCXDocumentPropertiesSubjectPropertyName stringValue:metadata[NSSubjectDocumentAttribute]]];
+	
+	if (metadata[NSAuthorDocumentAttribute])
+		[coreProperties addObject: [NSXMLElement elementWithName:RKDOCXDocumentPropertiesAuthorPropertyName stringValue:metadata[NSAuthorDocumentAttribute]]];
+	
+	if (metadata[NSKeywordsDocumentAttribute])
+		[coreProperties addObject: [NSXMLElement elementWithName:RKDOCXDocumentPropertiesKeywordsPropertyName stringValue:metadata[NSKeywordsDocumentAttribute]]];
+	
+	if (metadata[NSEditorDocumentAttribute])
+		[coreProperties addObject: [NSXMLElement elementWithName:RKDOCXDocumentPropertiesEditorPropertyName stringValue:metadata[NSEditorDocumentAttribute]]];
+	
+	NSXMLElement *timeAttribute = [NSXMLElement attributeWithName:RKDOCXDocumentPropertiesTimeAttributeName stringValue:RKDOCXDocumentPropertiesTimeTypeName];
+	
+	if (metadata[NSCreationTimeDocumentAttribute]) {
+		NSXMLElement *creationTimeElement = [NSXMLElement elementWithName:RKDOCXDocumentPropertiesCreationTimePropertyName stringValue:[self stringFromDate: metadata[NSCreationTimeDocumentAttribute]]];
+		[creationTimeElement addAttribute: [timeAttribute copy]];
+		[coreProperties addObject: creationTimeElement];
+	}
+	
+	if (metadata[NSModificationTimeDocumentAttribute]) {
+		NSXMLElement *modificationTimeElement = [NSXMLElement elementWithName:RKDOCXDocumentPropertiesModificationPropertyName stringValue:[self stringFromDate: metadata[NSModificationTimeDocumentAttribute]]];
+		[modificationTimeElement addAttribute: [timeAttribute copy]];
+		[coreProperties addObject: modificationTimeElement];
+	}
+	
+	if (metadata[NSCategoryDocumentAttribute])
+		[coreProperties addObject: [NSXMLElement elementWithName:RKDOCXDocumentPropertiesCategoryPropertyName stringValue:metadata[NSCategoryDocumentAttribute]]];
+	
+	return coreProperties;
+}
+
++ (NSString *)stringFromDate:(NSDate *)date
+{
+	NSDateFormatter *dateFormatter = [NSDateFormatter new];
+	dateFormatter.dateFormat = @"yyyy-MM-dd'T'hh:mm:ss'Z'";
+	dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation: @"UTC"];
+	
+	return [dateFormatter stringFromDate: date];
 }
 
 @end
