161e1ea8fc1dbb1dd154198cbe7dcfe546737b82
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-10 12:38:18 +0200

    Adds test case for orphan control

--- Tests/DOCX/Content Writer/RKDOCXParagraphAttributeWriterTest.m
+++ Tests/DOCX/Content Writer/RKDOCXParagraphAttributeWriterTest.m
@@ -158,7 +158,7 @@
 	NSDictionary *attributes = @{RKAdditionalParagraphStyleAttributeName: paragraphStyle};
 	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString: @"Keep-With-Following Test:"];
 	for (NSInteger index = 0; index < 9; index++) {
-		[attributedString appendAttributedString: [[NSAttributedString alloc] initWithString: @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]];
+		[attributedString appendAttributedString: [[NSAttributedString alloc] initWithString: @" Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]];
 	}
 	NSString *paragraph = @"\nLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.";
 	[attributedString appendAttributedString: [[NSAttributedString alloc] initWithString:paragraph attributes:attributes]];
@@ -168,6 +168,23 @@
 	NSData *converted = [document DOCX];
 	
 	[self assertDOCX:converted withTestDocument:@"keepwithfollowing"];
+}
+
+- (void)testParagraphElementWithOrphanControlAttribute
+{
+	RKAdditionalParagraphStyle *paragraphStyle = [RKAdditionalParagraphStyle new];
+	paragraphStyle.skipOrphanControl = YES;
+	NSDictionary *attributes = @{RKAdditionalParagraphStyleAttributeName: paragraphStyle};
+	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString: @"Orphan Control Test: "];
+	for (NSInteger index = 0; index < 9; index++) {
+		[attributedString appendAttributedString: [[NSAttributedString alloc] initWithString: @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est.\n"]];
+	}
+	[attributedString appendAttributedString: [[NSAttributedString alloc] initWithString: @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est.\n" attributes:attributes]];
+	
+	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
+	NSData *converted = [document DOCX];
+	
+	[self assertDOCX:converted withTestDocument:@"orphancontrol"];
 }
 
 - (void)testParagraphElementWithHyphenationAttribute
