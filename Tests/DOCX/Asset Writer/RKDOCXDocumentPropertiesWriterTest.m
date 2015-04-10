36999491df43802fb49656c593f849c08052dd2e
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-10 18:07:46 +0200

    Adds test case for document properties

---
+++ Tests/DOCX/Asset Writer/RKDOCXDocumentPropertiesWriterTest.m
@@ -0,0 +1,35 @@
+//
+//  RKDOCXDocumentPropertiesWriterTest.m
+//  RTFKit
+//
+//  Created by Lucas Hauswald on 10.04.15.
+//  Copyright (c) 2015 The Soulmen. All rights reserved.
+//
+
+#import "XCTestCase+DOCX.h"
+
+@interface RKDOCXDocumentPropertiesWriterTest : XCTestCase
+
+@end
+
+@implementation RKDOCXDocumentPropertiesWriterTest
+
+- (void)testCoreProperties
+{
+	RKDocument *document = [[RKDocument alloc] init];
+	NSDate *date = [NSDate dateWithTimeIntervalSince1970: 0];
+	NSDictionary *metadata = @{NSTitleDocumentAttribute: @"Title",
+							   NSSubjectDocumentAttribute: @"Subject",
+							   NSAuthorDocumentAttribute: @"Author",
+							   NSKeywordsDocumentAttribute: @"Key, Words",
+							   NSEditorDocumentAttribute: @"Editor",
+							   NSCreationTimeDocumentAttribute: date,
+							   NSModificationTimeDocumentAttribute: date,
+							   NSCategoryDocumentAttribute: @"Test Document"};
+	document.metadata = metadata;
+	NSData *converted = [document DOCX];
+	
+	[self assertDOCX:converted withTestDocument:@"coreproperties"];
+}
+
+@end
