c53f821775e80f39cf6f35de14f6495fe3a91e4e
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-10 11:45:28 +0200

    Adds hyphenation test case

--- Tests/DOCX/Content Writer/RKDOCXParagraphAttributeWriterTest.m
+++ Tests/DOCX/Content Writer/RKDOCXParagraphAttributeWriterTest.m
@@ -170,4 +170,18 @@
 	[self assertDOCX:converted withTestDocument:@"keepwithfollowing"];
 }
 
+- (void)testParagraphElementWithHyphenationAttribute
+{
+	RKAdditionalParagraphStyle *paragraphStyle = [RKAdditionalParagraphStyle new];
+	paragraphStyle.hyphenationEnabled = YES;
+	NSDictionary *attributes = @{RKAdditionalParagraphStyleAttributeName: paragraphStyle};
+	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"Hyphenation Test: You never know true beauty until you see Earth from space, or true terror until you hear someone knocking on the space station door from outside. You look through the porthole and see an astronaut, but all your crew is inside and accounted for. You use the comm to ask who it is and he says he’s Ramirez returning from a repair mission, but Ramirez is sitting right next to you in the command module and he’s just as confused as you are. When you tell the guy this over the radio he starts banging on the door louder and harder, begging you to let him in, saying he’s the real Ramirez. Meanwhile, the Ramirez inside with you is pleading to keep the airlock shut. It really puts life on Earth into perspective." attributes:attributes];
+	
+	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
+	document.hyphenationEnabled = YES;
+	NSData *converted = [document DOCX];
+	
+	[self assertDOCX:converted withTestDocument:@"hyphenation"];
+}
+
 @end
