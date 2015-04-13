de3554f99e9ef2abb0f78fd5296d2185bbac4fca
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-13 14:48:27 +0200

    Expands index of first page test to page number type test

--- Tests/DOCX/Content Writer/RKDOCXSectionWriterTest.m
+++ Tests/DOCX/Content Writer/RKDOCXSectionWriterTest.m
@@ -24,14 +24,15 @@
 	[self assertDOCX:converted withTestDocument:@"twocolumns"];
 }
 
-- (void)testSectionPropertiesElementWithIndexOfFirstPage
+- (void)testSectionPropertiesElementWithPageNumberType
 {
-	RKSection *section = [[RKSection alloc] initWithContent: [[NSAttributedString alloc] initWithString: @"This text is displayed in two columns. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est."]];
+	RKSection *section = [[RKSection alloc] initWithContent: [[NSAttributedString alloc] initWithString: @"This section starts with page number 42. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est."]];
 	section.indexOfFirstPage = 42;
+	section.pageNumberingStyle = RKPageNumberingRomanUpperCase;
 	RKDocument *document = [[RKDocument alloc] initWithSections: @[section]];
 	NSData *converted = [document DOCX];
 	
-	[self assertDOCX:converted withTestDocument:@"indexoffirstpage"];
+	[self assertDOCX:converted withTestDocument:@"pagenumbertype"];
 }
 
 @end
