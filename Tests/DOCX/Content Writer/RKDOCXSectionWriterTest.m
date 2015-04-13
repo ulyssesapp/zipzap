9dd640d1d9ef0973098e32c667ecb5d894bef639
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-13 16:02:49 +0200

    Adds test case for page size and orientation

--- Tests/DOCX/Content Writer/RKDOCXSectionWriterTest.m
+++ Tests/DOCX/Content Writer/RKDOCXSectionWriterTest.m
@@ -35,4 +35,15 @@
 	[self assertDOCX:converted withTestDocument:@"pagenumbertype"];
 }
 
+- (void)testSectionPropertiesElementWithPageSize
+{
+	RKSection *section = [[RKSection alloc] initWithContent: [[NSAttributedString alloc] initWithString: @"This page has the form of a square. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est."]];
+	RKDocument *document = [[RKDocument alloc] initWithSections: @[section]];
+	document.pageSize = CGSizeMake(500, 500);
+	document.pageOrientation = RKPageOrientationLandscape;
+	NSData *converted = [document DOCX];
+	
+	[self assertDOCX:converted withTestDocument:@"pagesize"];
+}
+
 @end
