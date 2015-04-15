0aa74c968cc60db5cfad069100aebc4a3941a1e4
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-15 15:46:24 +0200

    Adds test cases for image support

---
+++ Source/Utils/RKImage.h
@@ -0,0 +1,13 @@
+//
+//  RKImage.h
+//  RTFKit
+//
+//  Created by Lucas Hauswald on 15.04.15.
+//  Copyright (c) 2015 The Soulmen. All rights reserved.
+//
+
+#if TARGET_OS_IPHONE
+#define RKImage UIImage
+#else
+#define RKImage NSImage
+#endif
