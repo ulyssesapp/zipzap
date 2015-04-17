031e8e2ebc974d14663ef05d329649ae458e6df4
Lucas Hauswald <lucas@the-soulmen.com>
2015-04-17 11:39:34 +0200

    Adds missing documentation to image writer

--- Source/DOCX/Content Writer/RKDOCXImageWriter.h
+++ Source/DOCX/Content Writer/RKDOCXImageWriter.h
@@ -8,8 +8,15 @@
 
 #import "RKDOCXConversionContext.h"
 
+/*!
+ @abstract Generates a run element "<w:r>" with "w:drawing" as child element to be added to the parent paragraph.
+ @discussion See ISO 29500-1:2012: §20 (DrawingML).
+ */
 @interface RKDOCXImageWriter : NSObject
 
+/*!
+ @abstract Returns an XML element representing a run with an XML tree representing the given image attribute.
+ */
 + (NSXMLElement *)runElementWithImageAttachment:(RKImageAttachment *)imageAttachment inRunElement:(NSXMLElement *)runElement usingContext:(RKDOCXConversionContext *)context;
 
 @end
