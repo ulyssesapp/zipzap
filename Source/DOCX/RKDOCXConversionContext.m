3b67871ae4d6c5d09994d76bdc170bcedf37cf16
Friedrich Graeter <graeter@hydrixos.net>
2015-06-26 20:21:13 +0200

    Whitelisting further fonts

--- Source/DOCX/RKDOCXConversionContext.m
+++ Source/DOCX/RKDOCXConversionContext.m
@@ -147,9 +147,12 @@ NSString *RKDOCXConversionContextRelationshipIdentifierName	= @"ID";
 			@"Menlo Regular":			@YES,
 			@"Menlo Bold":				@YES,
 			@"Menlo Italic":			@YES,
-													   
+
+			@"Helvetica Neue UltraLight":	@YES,
+			@"Helvetica Neue Light":		@YES,			
+			
 			// The following fonts do not support full names on Word 2011. The automatic fallback in -isFullNameRequieredForFont shall not be used:
-			@"Helvetica Neue Italic":	@NO
+			@"Helvetica Neue Italic":		@NO
 			};
 }
 
