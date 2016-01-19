af66f1b9cb4d26a207afefadf4a922dcf22241a6
Lucas Hauswald <lucas@the-soulmen.com>
2016-01-19 15:43:02 +0100

    Removes two private methods from RKLinkWriter.h

--- Source/DOCX/Content Writer/Attribute Writer/RKDOCXLinkWriter.h
+++ Source/DOCX/Content Writer/Attribute Writer/RKDOCXLinkWriter.h
@@ -24,16 +24,4 @@ extern NSString *RKDOCXFieldLinkLastPartKey;
  */
 + (NSArray *)runElementsForLinkAttribute:(id)linkAttribute runType:(RKDOCXRunType)runType runElements:(NSArray *)runElements usingContext:(RKDOCXConversionContext *)context;
 
-/*!
- @abstract Converts the passed link attachment to a hyperlink element.
- @discussion linkAttribute must be either of type NSURL or NSString.
- */
-+ (NSXMLElement *)linkElementForAttribute:(id)linkAttribute usingContext:(RKDOCXConversionContext *)context;
-
-/*!
- @abstract Returns a dictionary containing all runs requiered for field hyperlinks.
- @discussion The dictionary contains two entries, one is an array filled with runs preceeding the actual link run, the other one is the element succeeding the link run.
- */
-+ (NSDictionary *)fieldHyperlinkRunElementsForLinkAttribute:(id)linkAttribute runType:(RKDOCXRunType)runType usingContext:(RKDOCXConversionContext *)context;
-
 @end
