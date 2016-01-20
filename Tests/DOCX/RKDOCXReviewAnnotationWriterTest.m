da726ea7e2391b8baf70a72d964d44c115827f07
Lucas Hauswald <lucas@the-soulmen.com>
2016-01-20 17:18:07 +0100

    Adds test cases for review comments (ULYSSES-5622)

--- Tests/DOCX/RKDOCXReviewAnnotationWriterTest.m
+++ Tests/DOCX/RKDOCXReviewAnnotationWriterTest.m
@@ -15,7 +15,7 @@
 
 @implementation RKDOCXReviewAnnotationWriterTest
 
-- (void)testParagraphsWithDeletedText
+- (void)testParagraphWithDeletedText
 {
 	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"This text should be marked as deleted." attributes:@{RKReviewAnnotationTypeAttributeName: @(RKReviewAnnotationTypeDeletion)}];
 	
@@ -24,17 +24,17 @@
 	[self assertDOCX:document withTestDocument:@"deleted"];
 }
 
-- (void)testParagraphsWithDeletedLink
+- (void)testParagraphWithDeletedLink
 {
 	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:@"This is a deleted link." attributes:@{RKReviewAnnotationTypeAttributeName: @(RKReviewAnnotationTypeDeletion)}];
-	[attributedString addAttribute:RKLinkAttributeName value:[NSURL URLWithString:@"http://example.org/"] range:NSMakeRange(18, 4)];
+	[attributedString addAttribute:RKLinkAttributeName value:[NSURL URLWithString: @"http://example.org/"] range:NSMakeRange(18, 4)];
 	
 	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
 	
 	[self assertDOCX:document withTestDocument:@"deletedlink"];
 }
 
-- (void)testParagraphsWithInsertedText
+- (void)testParagraphWithInsertedText
 {
 	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"This text should be marked as inserted." attributes:@{RKReviewAnnotationTypeAttributeName: @(RKReviewAnnotationTypeInsertion)}];
 	
@@ -43,14 +43,42 @@
 	[self assertDOCX:document withTestDocument:@"inserted"];
 }
 
-- (void)testParagraphsWithInsertedLink
+- (void)testParagraphWithInsertedLink
 {
 	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:@"This is an inserted link." attributes:@{RKReviewAnnotationTypeAttributeName: @(RKReviewAnnotationTypeInsertion)}];
-	[attributedString addAttribute:RKLinkAttributeName value:[NSURL URLWithString:@"http://example.org/"] range:NSMakeRange(20, 4)];
+	[attributedString addAttribute:RKLinkAttributeName value:[NSURL URLWithString: @"http://example.org/"] range:NSMakeRange(20, 4)];
 	
 	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
 	
 	[self assertDOCX:document withTestDocument:@"insertedlink"];
+}
+
+- (void)testParagraphWithComment
+{
+	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"This text has been commented on." attributes:@{RKReviewCommentAttributeName: [[NSAttributedString alloc] initWithString: @"This is the comment."]}];
+	
+	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
+	
+	[self assertDOCX:document withTestDocument:@"comment"];
+}
+
+- (void)testParagraphWithLinkInCommentedLink
+{
+	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:@"The comment of this link contains a link." attributes:@{RKReviewCommentAttributeName: [[NSAttributedString alloc] initWithString:@"Link" attributes:@{RKLinkAttributeName: [NSURL URLWithString: @"http://example.com/"]}]}];
+	[attributedString addAttribute:RKLinkAttributeName value:[NSURL URLWithString: @"http://example.org/"] range:NSMakeRange(20, 4)];
+	
+	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
+	
+	[self assertDOCX:document withTestDocument:@"commentlink"];
+}
+
+- (void)testParagraphWithCommentedInsertion
+{
+	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"This text has been inserted and commented on." attributes:@{RKReviewAnnotationTypeAttributeName: @(RKReviewAnnotationTypeInsertion), RKReviewCommentAttributeName: [[NSAttributedString alloc] initWithString: @"This is the comment."]}];
+	
+	RKDocument *document = [[RKDocument alloc] initWithAttributedString: attributedString];
+	
+	[self assertDOCX:document withTestDocument:@"commentedinsertion"];
 }
 
 @end
