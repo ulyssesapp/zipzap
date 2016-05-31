658c15982bdfe315b3e6d1254e33b489a3813428
Friedrich Graeter <graeter@hydrixos.net>
2016-05-31 14:50:41 +0200

    ULYSSES-6158 Fixes a performance issue due to high number of text runs

--- Source/PDF/CoreText Conversion/NSAttributedString+PDFCoreTextConversion.m
+++ Source/PDF/CoreText Conversion/NSAttributedString+PDFCoreTextConversion.m
@@ -43,6 +43,9 @@ NSMutableArray *NSAttributedStringCoreTextConverters;
 	if (context.document.hyphenationEnabled && context.document.locale.supportsHyphenation) {
 		NSMutableAttributedString *hyphenatedString = [convertedString mutableCopy];
 		
+		__block NSString *lastHyphentationCharacter;
+		__block NSInteger lastHyphenationRangeStart = 0;
+		
 		// Only hyphenate if supported by paragraph
 		[hyphenatedString enumerateAttribute:RKAdditionalParagraphStyleAttributeName inRange:NSMakeRange(0, hyphenatedString.length) options:0 usingBlock:^(RKAdditionalParagraphStyle *paragraphStyle, NSRange range, BOOL *stop) {
 			// Skip paragraphs that should not be hyphenated
@@ -54,11 +57,21 @@ NSMutableArray *NSAttributedStringCoreTextConverters;
 				// Add soft-hyphenation as hint for Core Text
 				[hyphenatedString.mutableString insertString:@"\u00ad" atIndex:index];
 				
-				// Remember the suggested hyphenation char for the given locale
-				[hyphenatedString addAttribute:RKHyphenationCharacterAttributeName value:suggestedSeparator range:NSMakeRange(index, 1)];
+				// Remember the suggested hyphenation char for the given locale as attribute. Try to create large attribute ranges, to reduce the number of runs for typesetting.
+				if (![lastHyphentationCharacter isEqualToString: suggestedSeparator]) {
+					if (lastHyphentationCharacter)
+						[hyphenatedString addAttribute:RKHyphenationCharacterAttributeName value:lastHyphentationCharacter range:NSMakeRange(lastHyphenationRangeStart, index - lastHyphenationRangeStart)];
+					
+					lastHyphentationCharacter = suggestedSeparator;
+					lastHyphenationRangeStart = index;
+				}
 			}];
 		}];
-				
+
+		// Setup hyphenation attribute for remaining range
+		if (lastHyphentationCharacter)
+			[hyphenatedString addAttribute:RKHyphenationCharacterAttributeName value:lastHyphentationCharacter range:NSMakeRange(lastHyphenationRangeStart, hyphenatedString.length - lastHyphenationRangeStart)];
+		
 		convertedString = hyphenatedString;
 	}
 	
