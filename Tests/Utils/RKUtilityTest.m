84cfa1b83f31613462c881385bc9780a14353028
Lucas Hauswald <Lucas@the-soulmen.com>
2016-10-04 16:27:56 +0200

    Fixes alignment using inferior whitespace

--- Tests/Utils/RKUtilityTest.m
+++ Tests/Utils/RKUtilityTest.m
@@ -14,29 +14,29 @@
 
 - (void)testMacroConversionPointsToTwips
 {
-    CGFloat originalValue = 93.826771653543303;
-    
-    NSInteger convertedValue = RKPointsToTwips(originalValue);
-    
-    NSAssert(convertedValue == 1876, @"Precision error when converting values using the RKPointsToTwips() macro. Converted value is %ld, should be %ld instead.", convertedValue, (long)(originalValue * 20));
+	CGFloat originalValue = 93.826771653543303;
+	
+	NSInteger convertedValue = RKPointsToTwips(originalValue);
+	
+	NSAssert(convertedValue == 1876, @"Precision error when converting values using the RKPointsToTwips() macro. Converted value is %ld, should be %ld instead.", convertedValue, (long)(originalValue * 20));
 }
 
 - (void)testMacroConversionTwipsToPoints
 {
-    CGFloat originalValue = 1876.535433070866;
-    
-    NSInteger convertedValue = RKTwipsToPoints(originalValue);
-    
-    NSAssert(convertedValue == 93, @"Precision error when converting values using the RKTwipsToPoints() macro. Converted value is %ld, should be %ld instead.", convertedValue, (long)(originalValue / 20));
+	CGFloat originalValue = 1876.535433070866;
+	
+	NSInteger convertedValue = RKTwipsToPoints(originalValue);
+	
+	NSAssert(convertedValue == 93, @"Precision error when converting values using the RKTwipsToPoints() macro. Converted value is %ld, should be %ld instead.", convertedValue, (long)(originalValue / 20));
 }
 
 - (void)testMacroConversionPointsToEMUs
 {
-    CGFloat originalValue = 93.826771653543303;
-    
-    NSInteger convertedValue = RKPointsToEMUs(originalValue);
-    
-    NSAssert(convertedValue == 1191600, @"Precision error when converting values using the RKPointsToEMUs macro. Converted value is %ld, should be %ld instead.", convertedValue, (long)(originalValue * 12700));
+	CGFloat originalValue = 93.826771653543303;
+	
+	NSInteger convertedValue = RKPointsToEMUs(originalValue);
+	
+	NSAssert(convertedValue == 1191600, @"Precision error when converting values using the RKPointsToEMUs macro. Converted value is %ld, should be %ld instead.", convertedValue, (long)(originalValue * 12700));
 }
 
 @end
