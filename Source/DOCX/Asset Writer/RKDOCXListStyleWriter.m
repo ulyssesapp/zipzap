edb5e0af20f999dda34805acf053da314d47e555
Friedrich Ruynat <friedrich@hydrixos.net>
2018-06-21 10:30:13 +0200

    Fixes wrong enumeration of abstract list styles

--- Source/DOCX/Asset Writer/RKDOCXListStyleWriter.m
+++ Source/DOCX/Asset Writer/RKDOCXListStyleWriter.m
@@ -73,8 +73,8 @@ NSString *RKDOCXListStyleEnumerationFormatUpperRomanAttributeValue		= @"upperRom
 	}
 	
 	// Abstract numberings
-	for (NSNumber *index in [context.listStyles.allKeys sortedArrayUsingSelector: @selector(compare:)]) {
-		[document.rootElement addChild: [self abstractNumberingElementFromListStyle:context.listStyles[index] usingContext:context]];
+	for (NSNumber *styleIndex in [context.listStyles.allKeys sortedArrayUsingSelector: @selector(compare:)]) {
+		[document.rootElement addChild: [self abstractNumberingElementFromListStyle:context.listStyles[styleIndex] index:styleIndex usingContext:context]];
 	}
 	
 	// Numbering instances
@@ -86,11 +86,11 @@ NSString *RKDOCXListStyleEnumerationFormatUpperRomanAttributeValue		= @"upperRom
 	[context addDocumentPartWithXMLDocument:document filename:[self packagePathForFilename:RKDOCXNumberingFilename folder:RKDOCXWordFolder] contentType:RKDOCXNumberingContentType];
 }
 
-+ (NSXMLElement *)abstractNumberingElementFromListStyle:(RKListStyle *)listStyle usingContext:(RKDOCXConversionContext *)context
++ (NSXMLElement *)abstractNumberingElementFromListStyle:(RKListStyle *)listStyle index:(NSNumber *)styleIndex usingContext:(RKDOCXConversionContext *)context
 {
 	// Numbering Identifier (§17.9.2)
 	NSXMLElement *abstractNumberingElement = [NSXMLElement elementWithName: RKDOCXListStyleAbstractNumberingElementName];
-	[abstractNumberingElement addAttribute: [NSXMLElement attributeWithName:RKDOCXListStyleAbstractNumberingAttributeName stringValue:@([context indexForListStyle: listStyle]).stringValue]];
+	[abstractNumberingElement addAttribute: [NSXMLElement attributeWithName:RKDOCXListStyleAbstractNumberingAttributeName stringValue:styleIndex.stringValue]];
 	
 	// Multi level type (§17.9.12)
 	NSXMLElement *multiLevelTypeElement = [NSXMLElement elementWithName: RKDOCXListStyleMultiLevelTypeElementName];
